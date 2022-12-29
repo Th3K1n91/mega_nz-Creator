6b471f1f77840a2e07b1b49c2a541b4700718611
Th3K1n91 <63650014+Th3K1n91@users.noreply.github.com>
2022-12-29 16:40:18 +0100

    fix

--- Modules/infos.py
+++ Modules/infos.py
@@ -1,21 +1,10 @@
+import random
 import requests
 
 def get_inofs():
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 OPR/80.0.4170.63"}
-    info = requests.get("https://outputter.io/full-identity/germany/", headers=headers).text
-    infonames = ["Name", "Address", "City", "Postcode", "Phone", "BIC", "IBAN"]
-    infos = []
-    for i in infonames:
-        rawinfo = info.split(f'row">{i}')[1].split('"top">')[1].split('<')[0]
-        infos.append(rawinfo)
+    firstnames = requests.get("https://www.randomlists.com/data/names-first.json").json()["data"]
+    lastnames = requests.get("https://www.randomlists.com/data/names-surnames.json").json()["data"]
 
     return {
-        "Name": infos[0],
-        "Address": infos[1],
-        "City": infos[2],
-        "Postcode": infos[3],
-        "Phone": infos[4],
-        "BIC": infos[5],
-        "IBAN": infos[6]
+        "Name": random.choice(firstnames) + " " + random.choice(lastnames),
     }
